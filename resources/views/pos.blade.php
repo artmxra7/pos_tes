b74045ead58ca85f7f50eb484fc237fe27e0b6dc
Erwin Rahayu <artmxarea@gmail.com>
2020-03-09 13:02:58 +0700

    add

--- resources/views/pos.blade.php
+++ resources/views/pos.blade.php
@@ -71,7 +71,7 @@
 					<input class="form-control" type="number" name="bayarkan" v-model="bayarkan" placeholder="Rp."><br>
                     <button class="form-control" onclick="window.location.reload(true)" style="cursor:pointer">CLEAR</button>
                     <hr class="col-lg-12">
-					<button v-on:click="bayar()" class="form-control"><h1>Charge Rp. @{{total_bayar}}</h1></button>
+					<button v-on:click="bayar()" class="form-control" style="background-color:#3B5EAB;"><h1>Charge Rp. @{{total_bayar}}</h1></button>
 				</div>
 			</div>
 
